0c79002e88c66d5b03c1518ebea5dacf4dcc32ca
KIM SOOBIN <xpsxm225@naver.com>
2023-06-25 20:24:16 +0900

    변수 교체

--- hon_gong_python/2/get_cicle_perimeter_area.py
+++ hon_gong_python/2/get_cicle_perimeter_area.py
@@ -11,3 +11,17 @@ perimeter = get_perimeter(input_radius)
 area = get_area(input_radius)
 
 print(f"입력한 반지름 : {input_radius}\n원의 둘레 : {perimeter}\n원의 넓이 : {area}")
+
+
+# -------------------------
+r = input('반지름 : ')
+r = float(r)
+
+#처리
+pi = 3.14
+perimeter_ = 2 * pi * r
+area_ = pi * ( r ** 2 )
+
+#출력
+print(f" 둘레 : {perimeter_}")
+print(f" 넓이 : {area_}")
---
+++ hon_gong_python/2/variable_replacement.py
@@ -0,0 +1,7 @@
+a = input("문자열 1 입력 : ")
+b = input("문자열 2 입력 : ")
+print(a,b)
+c=b
+b=a
+a=c
+print(a,b)
