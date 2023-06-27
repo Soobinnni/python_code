2ecaa3548e95ed77349ff29d9bdf75156ece7aae
KIM SOOBIN <xpsxm225@naver.com>
2023-06-27 20:45:16 +0900

    계절을 구하는 코드 - 포맷팅한 날짜의 계절 추가

--- hon_gong_python/3/classify_season.py
+++ hon_gong_python/3/classify_season.py
@@ -1,15 +1,27 @@
 from pytz import timezone
 from datetime import datetime
+from datetime import date
 
 month = datetime.now(timezone('Asia/Seoul')).month
 
 print(f"현재 {month}월")
 
-if 3 <= month <6 :
+# if 3 <= month <6 :
+#     print("봄입니다!")
+# elif 6 <= month <9 :
+#     print("여름입니다!")
+# elif 9<= month < 12 :
+#     print("가을입니다!")
+# else :
+#     print("겨울입니다!")
+
+month_ = date(2023, 12, 31).month
+
+if 3 <= month_ <6 :
     print("봄입니다!")
-elif 6 <= month <9 :
+elif month_ <9 :
     print("여름입니다!")
-elif 9<= month < 12 :
+elif month_ < 12 :
     print("가을입니다!")
 else :
     print("겨울입니다!")
