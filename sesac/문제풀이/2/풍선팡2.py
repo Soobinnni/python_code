909ea38a72ae02c9da442f6548a3093e9bfaaf4f
KIM SOOBIN <xpsxm225@naver.com>
2023-12-17 22:25:12 +0900

    새싹 문제 풀이(lv.1)

---
+++ sesac/문제풀이/2/풍선팡2.py
@@ -0,0 +1,23 @@
+t = int(input())
+
+for test_case in range(1, t+1):
+    n, m = map(int, input().split())
+    balloons = [list(map(int, input().split())) for num in range(n)]
+    ans = -1
+    pollen = 0
+
+    for i in range(n):
+        for j in range(m):
+            pollen = balloons[i][j]
+            if i > 0:
+                pollen += balloons[i-1][j]
+            if i < n-1:
+                pollen += balloons[i+1][j]
+            if j > 0 :
+                pollen += balloons[i][j-1]
+            if j < m-1:
+                pollen += balloons[i][j+1]
+
+            if ans < pollen: ans = pollen
+
+    print(f'#{test_case} {ans}')
