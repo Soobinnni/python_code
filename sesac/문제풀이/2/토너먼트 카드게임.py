885adf136bf37f326652c1057253b008242ceb23
KIM SOOBIN <xpsxm225@naver.com>
2023-12-31 15:22:46 +0900

    토너먼트 게임 문제 풀이

--- sesac/문제풀이/2/토너먼트 카드게임.py
+++ sesac/문제풀이/2/토너먼트 카드게임.py
@@ -1,46 +1,27 @@
-# https://swexpertacademy.com/main/learn/course/lectureProblemViewer.do
-
-
-# matching = list(enumerate(map(int, input().split())))  # enumerate 함수 사용하여 (참가번호, 가위바위보) 형태로 저장
-
-# 1은 가위, 2는 바위, 3은 보
-# 1-2 = 2 / 1-3 = 1 / 2-3 = 3
-# 만약 같은 카드인 경우 편의상 번호가 작은 쪽을 승자
-# array: (index, card)
-
-def game(me, you):
-    if (me[1] - you[1]) % 3 == 1:
-        return True
-    elif (me[1] - you[1]) % 3 == 2:
-        return False
-    elif me[0] > you[0]:
-        return False
+def game(player1, player2):
+    game_result = player1[1] - player2[1]
+    if not game_result:
+        return player1 if player1[0] < player2[0] else player2
+    if game_result in (-2, 1):
+        return player1
     else:
-        return True
+        return player2
 
-def game_result(array):
-    n = len(array)
-    if n == 1:
-        return array[0]
-    # if n == 2:
-    #     if game(array[0], array[1]):
-    #         return array[0]
-    #     else:
-    #         return array[1]
-
-    x = game_result(array[: n // 2 + 1])
-    y = game_result(array[n // 2 + 1:])
-    if game(x,y):
-        return x
+def tournament(team):
+    if len(team) == 1:
+        return team[0]
     else:
-        return y
+        l = tournament(team[:(len(team)+1) // 2])
+        r = tournament(team[(len(team)+1) // 2:])
+        return game(l, r)
 
-def main():
-    T = int(input())
-    for test_case in range(1, T+1):
-        N = int(input())
 
-        array = list(zip(range(1, N+1),map(int, input().split())))
+for test_case in range(1, int(input()) + 1):
+    n = int(input())
+    people_card = list(enumerate(map(int, input().split())))
+    winner = tournament(people_card)[0]+1
 
-        print(f"#{test_case} {game_result(array)[0]}")
-main()
+    print(f"#{test_case} {winner}")
+
+
+
